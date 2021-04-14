310d8bc22a84066b1eb6dd5a462ac381c8fa31bc
Duc Anh <nguyenducanh.ldb@gmail.com>
2021-04-14 11:17:09 +0700

    Validated

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -34,6 +34,32 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
 	req.body.id = shortid.generate();
+	// Mảng lưu lại tên các lỗi
+	var errors = [];
+
+	// Nếu không nhập tên
+	if(!req.body.name) {
+		// thêm lỗi vào mảng
+		errors.push('Name is required.');
+	}
+
+	// Nếu không nhập sdt
+	if(!req.body.phone) {
+		// thêm lỗi vào mảng
+		errors.push('Phone is required.');
+	}
+
+	// Nếu có lỗi trong mảng
+	if(errors.length) {
+		// trả về 2 giá trị là mảng lỗi và các giá trị đã nhập
+		res.render('users/create', {
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
+
+	// Nếu k có lỗi thì ghi vào db
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 };
